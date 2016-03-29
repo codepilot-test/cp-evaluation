b454df08ca49f7d2dc16c35fbfb25f6b2407b2e6
codepilot-test <codepilot-test@users.noreply.github.com>
2016-03-29 21:39:49 +0000

    Finishing!

--- site.js
+++ site.js
@@ -2,4 +2,18 @@ console.log("Hello world");
 
 $(document).ready(function() {
     console.log("rejoice - jquery is enabled!");
+    
+    $.getJSON('https://raw.githubusercontent.com/jeremywrnr/jeremywrnr.github.io/7cc04951d4b0421e664ebbc9e4aa6642e2c02dbb/weather.json', function(data) {
+        var $weatherpoop, avg=0, n=0;
+        $weatherpoop = $('#weather');
+        
+        var $items = data.map(function(item) {
+            avg += item.TemperatureF;
+            n++;
+            return $('<li></li>').text("Time: " + item.TimeEST + " / Temp: " + item.TemperatureF + "F");
+        })
+        avg = avg/n;
+        $weatherpoop.append($items);
+        $('#average').text("<Average Temperature: "+ avg);
+    })
 });
